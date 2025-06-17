66dcab779f629a881155c90de59ff00f25c859b4
Ahmed mo'men <123960549+C1ANCYSz@users.noreply.github.com>
2025-06-17 21:31:18 +0300

    Delete front-end directory

--- front-end/src/ui/Loader.jsx
+++
@@ -1,9 +0,0 @@
-function Loader() {
-  return (
-    <div className="from-primary-900 flex h-screen min-h-screen w-full items-center justify-center bg-gradient-to-br to-black p-8">
-      <div className="border-primary-500 h-64 w-64 animate-spin rounded-full border-t-2 border-b-2"></div>
-    </div>
-  );
-}
-
-export default Loader;
